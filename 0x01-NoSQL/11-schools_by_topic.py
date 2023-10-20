2b2366c6b4b59084b0b9fd43c7e26e9506ec6476
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-21 00:22:48 +0300

    Add task 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
-"""First commit."""
+"""Write a Python function that returns the list of school having a specific
+topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    :param mongo_collection: the pymongo collection object
+    :param topic: (string) will be topic searched
+    :return: (list) of schools having a specific topic
+    """
+
+    result = mongo_collection.find({"topics": topic})
+    schools = []
+
+    for school in result:
+        schools.append(school)
+
+    return schools
