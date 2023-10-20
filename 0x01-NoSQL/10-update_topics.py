abb23156ea330ba7859caa39dccf2a49d9f35200
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-21 00:09:40 +0300

    Add task 10 update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
-"""First commit."""
+"""Write a Python function that changes all topics of a school document based
+on the name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    changes all topics of a school document based on the name
+    :param mongo_collection: will be the pymongo collection object
+    :param name: (string) will be the school name to update
+    :param topics: list of strings) will be the list of topics approached in
+    the school
+    """
+
+    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
