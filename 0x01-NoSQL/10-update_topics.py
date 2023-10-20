96b9f972002b205e8820cae59e960eead5fb93f1
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-21 00:11:08 +0300

    Fix update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -13,4 +13,4 @@ def update_topics(mongo_collection, name, topics):
     the school
     """
 
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
