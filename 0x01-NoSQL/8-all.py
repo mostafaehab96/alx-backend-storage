ba696755a27cd0a24259c4bac2794f37b3a24245
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-20 23:49:33 +0300

    Solve task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 """First commit."""
+
+
+def list_all(mongo_collection):
+    """Returns a list of all documents in a collection"""
+    documents = mongo_collection.find()
+    all_documents = []
+
+    for document in documents:
+        all_documents.append(document)
+
+    return all_documents
