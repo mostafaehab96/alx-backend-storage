87a9889d15b2e069fb23d0ac069c7a79920737ec
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-21 00:00:23 +0300

    Solve task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
-"""First commit."""
+"""Insert a document in a collection."""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Insert a document in a collection."""
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
