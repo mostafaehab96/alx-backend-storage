49c3c90f5aae13eb1101f01b95dfb8ddae1688da
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-21 05:41:22 +0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,6 +2,7 @@
 """ implement a get_page function."""
 
 import requests
+import redis
 
 
 def get_page(url: str) -> str:
@@ -10,6 +11,12 @@ def get_page(url: str) -> str:
     :param url: the url to obtain it's HTML
     :return: HTML content
     """
+    client = redis.Redis()
+    client.incr(f'count:{url}')
     response = requests.get(url, allow_redirects=True, timeout=30)
+    cached_page = client.get(f'{url}')
+    if cached_page:
+        return cached_page.decode('utf-8')
     if response.status_code == 200:
+        client.set(f'{url}', response.text, 10)
         return response.text
